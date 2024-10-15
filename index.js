6dd2a56f39d1f194357d07d88d239aa23a147586
james chambers <jamcha123@localhost.localdomain>
2024-10-15 12:59:37 +0200

    gjorde css och lite js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import express from 'express';
+import express from 'express'
 import fs from 'fs';
+import http from 'http'
+import {Server} from 'socket.io'
 
 const app = express();
 app.use(express.static("public"));
@@ -16,4 +18,31 @@ app.get("/:file", (req, res) => {
         res.end();
     }) 
 })
-app.listen(8080, () => console.log("http://127.0.0.1:8080/index.html"))
+
+const server = http.createServer(app)
+
+function newItem(items, nums){
+    app.post("/" + items, (req, res) => {
+        res.writeHead(200, {'content-type': "text/html"});
+        res.write(nums)
+        res.end()
+    })
+}
+
+const io = new Server(server);
+io.on("connection", (socket) => {
+    socket.on("plus", (msg) => {
+        newItem("item1", "" + msg + "")
+    })
+    socket.on("minus", (msg) => {
+        newItem("item2", "" + msg + "")
+    })
+    socket.on("times", (msg) => {
+        newItem("item3", "" + msg + "")
+    })
+    socket.on("division", (msg) => {
+        newItem("item4", "" + msg + "")
+    })
+})
+
+server.listen(8080, () => console.log("http://127.0.0.1:8080/index.html"))
